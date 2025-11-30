5c80b2209d2569606a20420da06a17e07fb622af
Frost <notshivamsingh@gmail.com>
2025-12-01 00:13:25 +0530

    Index Connection

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import  express from "express"
+
+const app = express
+
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        app.on("error",(error)=>{
+
+            console.log("error",error)
+            throw error;
+
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening${process.env.PORT}`)
+        })
+        
+    } catch (error) {
+        console.error("Error connecting to the database", error);
+        throw error;
+        
+    }
+})()
