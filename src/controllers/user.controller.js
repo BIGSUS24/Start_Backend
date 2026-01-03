caf7a5f6d2a00b148c1ef725d745eacf71a89929
Frost <notshivamsingh@gmail.com>
2026-01-03 18:18:30 +0530

    setting up Logic And Going with Algorithm For Controller user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiErrors} from "../utils/apiErrors.js"
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 const registerUser = asyncHandler(async (req,res) => {
 
@@ -11,7 +14,39 @@ const registerUser = asyncHandler(async (req,res) => {
 //remove password from response
 //check for user creation success
 //return result 
+
+const {fullname,email,username,password}= req.body
+console.log(email,password);
+//validation
+ if ([fullname,email,username,password].some((field)=>
+field?.trim()==="")) {
+
+    throw new ApiErrors(400,"all Feilds are required")
     
+ }
+ //check if user exists 
+
+ const existedUser = User.findOne(
+    {
+        $or:[{username},{email}]//returns true of false
+    }
+ )
+    if (existedUser) {
+        
+        throw new ApiErrors(409,"Existing username or Email")
+  
+    }
+
+    const AvatarLocalPath = req.files?.avatar[0].Path;
+    const coverImageLocalPath = req.files?.coverImage[0].Path;
+
+     if (AvatarLocalPath) {
+        
+        throw new ApiErrors(40,"Avatar is Required")
+  
+    }
+    const Avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImageLocal = await uploadOnCloudinary(coverImageLocalPath)
 })
 
 export {registerUser}
