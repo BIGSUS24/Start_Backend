ba3d9545dcdecb20e90d99945b819480f8fd432a
Frost <notshivamsingh@gmail.com>
2026-01-03 15:15:04 +0530

    Boilerplate for backend

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,15 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req,res) => {
 
-    res.status(200).json({
-        message:"ok"
-    })
+//get user data from frontend
+//validate user data if empty
+//check if user already exists
+//check for image and avatar upload
+//upload them to cloudinary
+//create user in db
+//remove password from response
+//check for user creation success
+//return result 
     
 })
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router() 
 
 router.route("/register").post(registerUser)
 
-
+//uploading files
 
 
 
